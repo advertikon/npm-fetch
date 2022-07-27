de15ad6087d14372a86f4f543a4000551bd464e0
Maxim Bogochas <bogochunas@gmail.com>
2022-07-27 19:30:27 +0300

    fix: change trace header

--- fetch.ts
+++ fetch.ts
@@ -1,12 +1,19 @@
 import fetch, { RequestInfo, RequestInit } from 'node-fetch';
 import { Request } from 'express';
 
-export default function logFetch (req: Request) {
+interface FetchRequest extends Request {
+    req_id: string;
+}
+
+export default function logFetch (req: FetchRequest) {
     const request = req;
 
     return function doFetch (url: RequestInfo, init: RequestInit = { headers: {} }) {
-        // @ts-ignore
-        init.headers['x-orig-req'] = request.req_id;
+        if (init.headers) {
+            // @ts-ignore
+            init.headers['x-req-id'] = request.req_id;
+        }
+
         return fetch(url, { ...(init || {}), });
     }
 }
