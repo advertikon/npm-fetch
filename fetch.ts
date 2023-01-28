884981cd60bdcd982d18e04600a9b717fb9ddea6
Maxim Bogochas <bogochunas@gmail.com>
2023-01-28 18:04:05 +0200

    perf: do not handle respose
    BREAKING CHANGE: response needs to be handled by outside code

--- fetch.ts
+++ fetch.ts
@@ -1,7 +1,6 @@
-import fetch, { RequestInfo, RequestInit, Response } from 'node-fetch';
+import fetch, { RequestInfo, RequestInit } from 'node-fetch';
 import { ulid } from 'ulid';
 import { Request } from 'express';
-import { FetchError } from '@bogochunas/error-handler';
 
 interface FetchRequest extends Request {
     req_id: string;
@@ -11,14 +10,6 @@ export function logFetch (req?: FetchRequest) {
     return function doFetch (url: RequestInfo, init: RequestInit = {}) {
         init.headers = { ...(init.headers || {}), 'x-req-id': req?.req_id || ulid() };
 
-        return fetch(url, init).then(processResponse);
+        return fetch(url, init);
     }
-}
-
-function processResponse (resp: Response) {
-    if (!resp.ok) {
-        throw FetchError(resp);
-    }
-
-    return resp.json();
 }
